e7b87b67e4d9a2bd9a352d01e5e8cbacc951df1d
Adeyemi Adejumo <adejumoadeyemi32@gmail.com>
2023-01-22 15:28:09 +0000

    requirement

--- sort.h
+++ sort.h
@@ -12,9 +12,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_list(const listint_t *list);
@@ -23,4 +23,4 @@ void selection_sort(int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 
-#endif
+#endif /*SORT_H*/
