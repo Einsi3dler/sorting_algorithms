2deb6653a1cfe62c280ac0fc48ae41fb925bb213
sylvester_divine <sylvesterdivine@outlook.com>
2023-01-22 20:11:22 +0000

    file create

---
+++ 3-quick_sort.c
@@ -0,0 +1,7 @@
+#include "sort.h"
+/**
+ * quick_sort - this sorts the list using the quick sort method
+ * @array - this contains the array to be sorted
+ * @size - this contains the size of the array
+ */
+void quick_sort(int *array, size_t size);
