6e0b2284b3037548d518303b19dfd9bfa13f9ef5
sylvester_divine <sylvesterdivine@outlook.com>
2023-01-23 23:39:46 +0000

    quick sort build untestested

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,7 +1,59 @@
 #include "sort.h"
+
+int partition(int *array[], int low, int high, size_t size)
+{
+	int pivot;
+	int i,j,temp;
+
+	pivot = array[high];
+	i = (low-1);
+	for (j = low; j<=high - 1; j++)
+	{
+		if (array[j] <= pivot)
+		{
+			i++;
+			if (j != i)
+			{
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+	if (i+1 != high)
+	{
+		temp = array[i+1];
+		array[i+1] = array[high];
+		array[high] = temp;
+	}
+	return (i+1);
+}
+
+
+/**
+ * sort - this sorts the array recursively
+ * @array: the array to be sorted
+ * @low: low point of the array
+ * @high: high point of the array
+ */
+void sort(int *array, int low, int high, size_t size)
+{
+	if (low < high)
+	{
+		int pa;
+
+		pa = partition(array, low, high, size_t size);
+		sort(array, low, pa-1);
+		sort(array, pa+1, high);
+	}
+}
 /**
  * quick_sort - this sorts the list using the quick sort method
  * @array - this contains the array to be sorted
  * @size - this contains the size of the array
  */
-void quick_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size)
+{
+	sort(array, o, size-1);
+}
--- sort.h
+++ sort.h
@@ -17,10 +17,17 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/*this are functions that access variables*/
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+
+/*this are sort functions*/
 void selection_sort(int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void quick_sort(int *array, size_t size);
 
+/*util functions*/
+int partition(int *array, int low, int high, size_t size);
+void sort(int *array, int low, int high, size_t size);
 #endif /*SORT_H*/
